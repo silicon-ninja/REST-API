b6ad043c0b8f6bb9eef50937fb63c442c2d61810
Srikanth-Kandarp <srikanthkandarp23@gmail.com>
2021-04-25 19:03:54 +0530

    Updated

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-const express = require('express'); // Importing the Package 
+const express = require('express'); // Importing the Package
 const app = express(); // Executing the Package
 const mongoose = require('mongoose');
-require('dotenv/config');
+require('dotenv/config'); // For using sensitive information like Mongo DB Key
 
-const homeroutes = require('./routes/home') 
+const homeroutes = require('./routes/home');
 const bodyparser = require('body-parser');
 
-
-// JSON Incoming Request 
+// JSON Incoming Request
 
 app.use(bodyparser.json());
 
-// Assigning Routes 
-app.use('/',homeroutes);
+// Assigning Routes
+app.use('/requests', homeroutes);
 
-// Database 
-mongoose.connect(process.env.DB_CONNECTION,
-        { useNewUrlParser: true },
-            ()=> console.log('Database Connetion Established'));
-
-
-
+// Database
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('Database Connetion Established')
+);
 
 // Listening to the Messeages
 
-app.listen('port',process.env.PORT || 8800);
+app.listen('port', process.env.PORT || 8800);
--- routes/home.js
+++ routes/home.js
@@ -1,80 +1,78 @@
-const express = require('express'); // Importing the Express Dependency 
+const express = require('express'); // Importing the Express Dependency
 const router = express.Router(); // Accessing the Router
 const posts = require('../modules/post'); // Accessing the modules folder
 const { request } = require('express');
 
-// Reply from the server.
+// Simple Get Request where you can retrive the data from your DB
 
 router.get('/', async (req, res) => {
-    try {
-        const Post = await posts.find()
-        res.json(Post)
-    } catch (err) {
-        res.json({ message: err });
-    }
+  try {
+    const Post = await posts.find();
+    res.json(Post);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-// Search for the specific post 
-router.get('/xw', async (req, res) => {
-try {
-        const post = await posts.findById(req.query.id);
-        res.json(post)
-    }
-    catch (err) {
-    res.json({ message: err })};
-    });
+// Simple Get Request where you can retrive the data from your DB by its ID
 
+router.get('/', async (req, res) => {
+  try {
+    const post = await posts.findById(req.query.id); // Requesting the _id
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-// Delete the specific post 
+// Simple Delete Request where you can delete the data from your DB by its ID
 
 router.delete('/', async (req, res) => {
-   console.log(req.query.id);
-    try {
-        const removedNote = await posts.remove({ _id: req.query.id });
-        res.json(removedNote);
-    }
-    catch (err) {
-        res.json({ message: err });
-    }
+  console.log(req.query.id); // Requesting the _id
+  try {
+    const removedNote = await posts.remove({ _id: req.query.id });
+    res.json(removedNote);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-// Update he specific post 
+// Simple Patch Request where you can Update the data from your DB by its ID
 
 router.patch('/', async (req, res) => {
-    try {
-        const updatedNote = await posts.updateMany({ _id: req.query.id },
-            {
-                $set: {
-                    title: req.body.title,
-                    description: req.body.description
-                }
-            }, { multi: true });
-    
-            
+  try {
+    const updatedNote = await posts.updateMany(
+      { _id: req.query.id }, // Requesting the _id
+      {
+        $set: {
+          title: req.body.title, // Requesting the Title
+          description: req.body.description, // Requesting the Description
+        },
+      },
+      { multi: true }
+    );
+
     res.json(updatedNote);
-    }
-    catch (err) {
-        res.json({ message: err });
-    }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-// Submitting the Post 
-router.post('/', async (req, res) => { // Response
-    console.log(req.body);
-    const post = new posts({
-        title: req.body.title, // Requesting the Title
-        description: req.body.description// Requesting the Description
-        
-    })
-    try {
-        const savedpost = await post.save() // Saving it to the Darabase
-        res.json(savedpost);
-        console.log("Saved"); // Response in JSON
-    }
-    catch (err) {
-        res.json({ message: err }); // Catch the Error
-    }
+// Simple Post Request where you can insert data to your DB with its schema
 
-}); 
+router.post('/', async (req, res) => {
+  // Response
+  const post = new posts({
+    title: req.body.title, // Requesting the Title
+    description: req.body.description, // Requesting the Description
+  });
+  try {
+    const savedpost = await post.save(); // Saving it to the Darabase
+    res.json(savedpost);
+    console.log('Saved'); // Response in JSON
+  } catch (err) {
+    res.json({ message: err }); // Catch the Error
+  }
+});
 
-module.exports = router; // Exporting the Router Variable 
+module.exports = router; // Exporting the Router Variable
